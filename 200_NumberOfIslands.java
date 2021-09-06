0f9600a619c2d3f34999e51fb956ff983b8ba5c0
psuraj776 <33577824+psuraj776@users.noreply.github.com>
2021-09-06 10:34:54 +0530

    Add files via upload

---
+++ 200_NumberOfIslands.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]=='1'){
+                    dfs(i,j,grid);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    
+    public void dfs(int i,int j,char[][] grid){
+        if(i>=grid.length || i<0 || j>=grid[0].length || j<0 || grid[i][j]=='0'){
+            return;
+        }
+        if(grid[i][j] == '1'){
+            grid[i][j]='0';
+            dfs(i+1,j,grid);
+            dfs(i,j+1,grid);
+            dfs(i,j-1,grid);
+            dfs(i-1,j,grid);
+        }
+    }
+}
