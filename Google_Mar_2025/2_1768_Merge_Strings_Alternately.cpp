0ced1f0ddf4d67b2becc965c2eb106380abfa5ff
psuraj776 <33577824+psuraj776@users.noreply.github.com>
2025-02-14 17:47:52 +0530

    Create 2_1768_Merge_Strings_Alternately.cpp

---
+++ Google_Mar_2025/2_1768_Merge_Strings_Alternately.cpp
@@ -0,0 +1,27 @@
+/* Time O(max(n1, n2)) as we are iterating to the max len of wither word1 or word2
+*  Space O(1) we are not using any extra space other than result string
+*
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+string mergeAlternately(string word1, string word2) {
+    int n1=word1.size();
+    int n2=word2.size();
+    string res;
+    
+    for(int i=0;i<max(n1,n2); i++) {
+        if(i<n1) res += word1[i];
+        if(i<n2) res += word2[i];
+    }
+    return res;
+}
+int main() {
+    string word1="indore";
+    string word2="jaipur";
+    string res = mergeAlternately(word1, word2);
+    cout<<res<<endl;
+    return 0;
+}
